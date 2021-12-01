17715a3696e1ccab5931dd62c2c81b83dd1376f7
gregoryjohncarter <gregoryjohncarter@gmail.com>
2021-12-01 17:04:07 -0500

    fixed bracket indexing

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const generatePassword = () => {
 
+  let pwArray = [''];
 //ask what length they would like
 var pwLengthPrompt = window.prompt("What character length would you like for your password? Please enter a number at least 8 characters long and no longer than 128");
 
@@ -33,40 +34,38 @@ debugger;
 const randomCharacter1 = () => { 
   const lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  return Math.floor(Math.random() * (lowercaseArray.length - 1));
+  return lowercaseArray[Math.floor(Math.random() * (lowercaseArray.length - 1))];
   
 }
 
 const randomCharacter2 = () => { 
   const uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   
-  return Math.floor(Math.random() * (uppercaseArray.length - 1)); 
+  return uppercaseArray[Math.floor(Math.random() * (uppercaseArray.length - 1))]; 
 }
 const randomCharacter3 = () => { 
   const numericArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
   
-  return Math.floor(Math.random() * (numericArray.length - 1));
+  return numericArray[Math.floor(Math.random() * (numericArray.length - 1))];
 }
 const randomCharacter4 = () => { 
   const specialArray = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '['];
   
-  return Math.floor(Math.random() * (specialArray.length - 1));
+  return specialArray[Math.floor(Math.random() * (specialArray.length - 1))];
 }
 const randomCharacter5 = () => { 
   const allArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '['];
   
-  return Math.floor(Math.random() * (allArray.length - 1));
+  return allArray[Math.floor(Math.random() * (allArray.length - 1))];
 }
 
     
 //push a random character from the chosen array--  'pwLength.length' amount of times
     switch (pwTypePrompt) {
-
-      case 1:
-        let pwArray = [''];
-        for (let i = pwLength.length - 1; i >= 0; i--) {
+       case 1:
+        for (let i = 0; i < pwLength.length; i++) {
          
           pwArray.push(randomCharacter1());
       }
@@ -74,7 +73,6 @@ const randomCharacter5 = () => {
       break;
 
       case 2:
-        let pwArray = [''];
         for (let i = pwLength.length - 1; i >= 0; i--) {
     
           pwArray.push(randomCharacter2());
@@ -83,7 +81,6 @@ const randomCharacter5 = () => {
       break;
 
       case 3:
-        let pwArray = [''];
         for (let i = pwLength.length - 1; i >= 0; i--) {
       
           pwArray.push(randomCharacter3());
@@ -92,8 +89,6 @@ const randomCharacter5 = () => {
       break;
 
       case 4:
-        let pwArray = [''];
-       
         for (let i = pwLength.length - 1; i >= 0; i--) {
          
           pwArray.push(randomCharacter4());
@@ -102,8 +97,6 @@ const randomCharacter5 = () => {
       break;
 
       case 5:
-        let pwArray = [''];
-
         for (let i = pwLength.length - 1; i >= 0; i--) {
          
           pwArray.push(randomCharacter5());
